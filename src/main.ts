af12ccc3f24e69d23182406d2e7fab636248b0f1
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-22 20:50:48 +0300

    Display temperature in celcius

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import "./scss/style.scss";
 import { format } from "date-fns";
 import getById from "./ts/util";
-const [searchBar, searchButton, location, date, time, weatherText] = getById(
+const [
+  searchBar,
+  searchButton,
+  location,
+  date,
+  time,
+  temperature,
+  weatherText,
+] = getById(
   "search-bar",
   "search-button",
   "location",
   "date",
   "time",
+  "temperature",
   "weather-text"
 ) as HTMLElement[];
 
@@ -16,12 +25,12 @@ const [searchBar, searchButton, location, date, time, weatherText] = getById(
       `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
     );
     const weatherData = await response.json();
+    location.textContent = weatherData.location.name;
     const dateTimeData = new Date(weatherData.location.localtime);
     date.textContent = format(dateTimeData, `eeee, do MMM ''yy`);
-    console.log(date);
     time.textContent = format(dateTimeData, `h:mmaaa`);
-    location.textContent = weatherData.location.name;
     weatherText.textContent = weatherData.current.condition.text;
+    temperature.textContent = `${weatherData.current.temp_c}°C`;
   } catch (error) {
     console.log(error);
   }
