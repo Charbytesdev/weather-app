7182aec5a9abb4e0eed4fa7a38149e48e189b6d3
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-22 21:17:42 +0300

    Refactor and add functional swap temperature button

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,30 @@
 import "./scss/style.scss";
 import { format } from "date-fns";
 import getById from "./ts/util";
+
+type apiTempData = {
+  temp_c: string;
+  temp_f: string;
+};
+
 const [
   searchBar,
   searchButton,
-  location,
-  date,
-  time,
-  temperature,
   weatherText,
+  locationText,
+  dateText,
+  timeText,
+  temperatureText,
+  temperatureSwapButton,
 ] = getById(
   "search-bar",
   "search-button",
+  "weather-text",
   "location",
   "date",
   "time",
   "temperature",
-  "weather-text"
+  "swap-temperature"
 ) as HTMLElement[];
 
 (searchButton as HTMLButtonElement).onclick = async () => {
@@ -25,13 +33,27 @@ const [
       `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
     );
     const weatherData = await response.json();
-    location.textContent = weatherData.location.name;
     const dateTimeData = new Date(weatherData.location.localtime);
-    date.textContent = format(dateTimeData, `eeee, do MMM ''yy`);
-    time.textContent = format(dateTimeData, `h:mmaaa`);
+
     weatherText.textContent = weatherData.current.condition.text;
-    temperature.textContent = `${weatherData.current.temp_c}°C`;
+    locationText.textContent = weatherData.location.name;
+    dateText.textContent = format(dateTimeData, `eeee, do MMM ''yy`);
+    timeText.textContent = format(dateTimeData, `h:mmaaa`);
+    temperatureText.textContent = `${weatherData.current.temp_c}°C`;
+    temperatureSwapButton.textContent = "Display °F";
+    temperatureSwapButton.onclick = () => swapTemperatures(weatherData.current);
   } catch (error) {
     console.log(error);
   }
 };
+
+function swapTemperatures(data: apiTempData) {
+  const currentTemp = temperatureText.textContent?.split("°")[1];
+  const isCelsius = currentTemp == "C";
+
+  temperatureText.textContent = isCelsius
+    ? `${data.temp_f}°F`
+    : `${data.temp_c}°C`;
+
+  temperatureSwapButton.textContent = isCelsius ? "Display °C" : "Display °F";
+}
