78744d5c472bb71002ce29b3a37746769f8e69ca
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-22 19:36:50 +0300

    Print search weather in console

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./scss/style.scss";
+import searchBar from "./ts/searchBar";
+import searchButton from "./ts/searchButton";
+
+(searchButton as HTMLButtonElement).onclick = async () => {
+  const weatherPromise = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
+  );
+
+  const weatherPromiseJSON = await weatherPromise.json();
+  console.log(weatherPromiseJSON.current.condition.text);
+};
