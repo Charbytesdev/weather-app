5935d17ae35ea54efd458aea0eff327511404ba8
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-22 19:48:48 +0300

    Display search-weather on screen
    Display the weather in a div element on screen with text(ex: Partly cloudy)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import "./scss/style.scss";
 import searchBar from "./ts/searchBar";
 import searchButton from "./ts/searchButton";
+import weatherText from "./ts/weatherText";
 
 (searchButton as HTMLButtonElement).onclick = async () => {
-  const weatherPromise = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
-  );
+  try {
+    const weatherPromise = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
+    );
 
-  const weatherPromiseJSON = await weatherPromise.json();
-  console.log(weatherPromiseJSON.current.condition.text);
+    const weatherPromiseJSON = await weatherPromise.json();
+    weatherText.textContent = weatherPromiseJSON.current.condition.text;
+  } catch (error) {
+    console.log(error);
+  }
 };
