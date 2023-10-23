fdbea383684db608977c7e08ac80d4da26038564
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-23 15:21:42 +0300

    Display feels-like, humidity, wind-speed & name of 3 proceeding days
    Display feels-like, humidity, wind-speed for current day, refactor naming and code for readability and display the name of the next 3 days from the current day.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import "./scss/style.scss";
 import { format } from "date-fns";
 import getById from "./ts/util";
 
-type apiTempData = {
+type APITempData = {
   temp_c: string;
   temp_f: string;
+  feelslike_c: string;
+  feelslike_f: string;
+};
+
+type ForecastDayElement = {
+  date: string;
+  temp: {
+    maxtemp_c: string;
+    maxtemp_f: string;
+    mintemp_c: string;
+    mintemp_f: string;
+  };
 };
 
 const [
-  searchBar,
+  searchBarInput,
   searchButton,
   weatherText,
   locationText,
@@ -17,6 +29,12 @@ const [
   temperatureText,
   temperatureSwapButton,
   weatherImg,
+  feelsLikeText,
+  humidityText,
+  windSpeedText,
+  nextDay1,
+  nextDay2,
+  nextDay3,
 ] = getById(
   "search-bar",
   "search-button",
@@ -26,30 +44,59 @@ const [
   "time",
   "temperature",
   "swap-temperature-button",
-  "weather-img"
+  "weather-img",
+  "feels-like",
+  "humidity",
+  "wind-speed",
+  "next-day1",
+  "next-day2",
+  "next-day3"
 ) as HTMLElement[];
 
 (searchButton as HTMLButtonElement).onclick = async () => {
+  getCurrentWeather();
+};
+
+async function getCurrentWeather() {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=360903de59304a0ea59113502232010&q=${searchBar.value}`
+      `http://api.weatherapi.com/v1/forecast.json?key=360903de59304a0ea59113502232010&days=3&q=${searchBarInput.value}`
     );
-    const weatherData = await response.json();
-    const dateTimeData = new Date(weatherData.location.localtime);
-    (weatherImg as HTMLImageElement).src = weatherData.current.condition.icon;
-    weatherText.textContent = weatherData.current.condition.text;
-    locationText.textContent = weatherData.location.name;
+    const {
+      forecast: { forecastday },
+      current,
+      current: {
+        temp_c,
+        humidity,
+        feelslike_c,
+        wind_kph,
+        condition: { icon, text },
+      },
+      location: { name, localtime },
+    } = await response.json();
+    const dateTimeData = new Date(localtime);
+    (weatherImg as HTMLImageElement).src = icon;
+    weatherText.textContent = text;
+    locationText.textContent = name;
     dateText.textContent = format(dateTimeData, `eeee, do MMM ''yy`);
-    timeText.textContent = format(dateTimeData, `h:mmaaa`);
-    temperatureText.textContent = `${weatherData.current.temp_c}°C`;
-    temperatureSwapButton.textContent = "Display °F";
-    temperatureSwapButton.onclick = () => swapTemperatures(weatherData.current);
+    swapTemperatures(current, localtime);
+    temperatureSwapButton.onclick = () => swapTemperatures(current, localtime);
+    humidityText.textContent = `${humidity}%`;
+    windSpeedText.textContent = `${wind_kph}km/h`;
+    [nextDay1.textContent, nextDay2.textContent, nextDay3.textContent] =
+      forecastday.map((forecastDayElement: ForecastDayElement) =>
+        format(new Date(forecastDayElement.date), "EEEE")
+      );
+    forecastday.map(
+      (forecastdayElement: ForecastDayElement) =>
+        forecastdayElement.temp.maxtemp_c
+    );
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-function swapTemperatures(data: apiTempData) {
+function swapTemperatures(data: APITempData, localtime: string) {
   const currentTemp = temperatureText.textContent?.split("°")[1];
   const isCelsius = currentTemp == "C";
 
@@ -58,4 +105,13 @@ function swapTemperatures(data: apiTempData) {
     : `${data.temp_c}°C`;
 
   temperatureSwapButton.textContent = isCelsius ? "Display °C" : "Display °F";
+
+  const dateTimeData = new Date(localtime);
+  timeText.textContent = isCelsius
+    ? format(dateTimeData, `h:mmaaa`)
+    : format(dateTimeData, `HH:mm`);
+
+  feelsLikeText.textContent = isCelsius
+    ? `${data.feelslike_f}°F`
+    : `${data.feelslike_c}°C`;
 }
